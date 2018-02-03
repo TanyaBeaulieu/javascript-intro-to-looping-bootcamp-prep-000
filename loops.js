66a7a541b98047b6fdf2209ea0058cc545d23818
Tanya Beaulieu <parkcitycloset@yahoo.com>
2018-02-03 17:46:26 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,14 @@
+function forLoop(array) { 
+  for (var i = 0; i < 25; i++) {
+    j = i + 1;
+    if (j === 1) {
+      array.push("I am 1 strange loop.")
+      //  console.log(i);
+      }
+    else {
+      array.push("I am " + j + " strange loops.")
+      //  console.log(i);
+      } 
+  }
+  //return array;
+}
